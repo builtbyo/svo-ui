95a3417c7639bbd9bb132ae58960159b967f936f
Built By O <builtbyo@omerekmen.com>
2025-09-01 16:58:56 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import '../app.css';
+    
+    let { children } = $props();
+ 
+</script>
+
+<svelte:head>
+    <title>Your Dynamic Title</title>
+    <meta name="description" content="Your dynamic description" />
+</svelte:head>
+
+
+{@render children()}
